7637baea004f2c33ab3a7a4c82382a20374ec5ad
rosancoderian <rosancoderian@gmail.com>
2018-05-31 16:14:03 +0700

    update ticker.js

--- src/game.js
+++ src/game.js
@@ -1,49 +1,7 @@
-import Emitter from './emitter.js'
+import Ticker from './ticker.js'
 import Stage from './stage.js'
 import Assets from './assets.js'
-import Keyboard from './keyboard.js';
-
-class Ticker extends Emitter {
-	constructor () {
-		super()
-		this.fps = 60
-		this._raf = null
-		this._last = null
-		this._now = null
-		this._dt = null
-		this._accumulator = 0
-		this._delta = 1E3 / this.fps
-		this._step = 1 / this.fps
-
-	}
-
-	start () {
-		requestAnimationFrame(this._update.bind(this))
-		this.emit('start')
-	}
-
-	stop () {
-		if (this._raf) {
-			cancelAnimationFrame(this._raf)
-			this.emit('stop')
-		}
-	}
-	
-	_update () {
-		this._raf = requestAnimationFrame(this._update.bind(this))
-		this._now = performance.now()
-		this._dt = this._now - this._last
-		this._last = this._now
-		if (this._dt > 1E3) {
-			return
-		}
-		this._accumulator += this._dt
-		while (this._accumulator >= this._delta) {
-			this.emit('update', this._step)
-			this._accumulator -= this._delta;
-		}
-	}
-}
+import Keyboard from './keyboard.js'
 
 let assets = new Assets()
 
