e2948fa581cf7ce7b4e62a156c6f031b7793ea4c
rosancoderian <rosancoderian@gmail.com>
2018-06-01 10:15:09 +0700

    update stage.js

--- src/libs/stage.js
+++ src/libs/stage.js
@@ -6,14 +6,16 @@ export default class Stage extends Emitter {
         this.canvas = document.createElement('canvas')
         this.canvas.width = width
         this.canvas.height = height
-        this.ctx = this.canvas.getContext('2d');
-        this.ctx.imageSmoothingEnabled = false;
-        document.body.appendChild(this.canvas);
+        this.width = width
+        this.height = height
+        this.ctx = this.canvas.getContext('2d')
+        this.ctx.imageSmoothingEnabled = false
+        document.body.appendChild(this.canvas)
     }
 
-    clear (fill = '#000', x = 0, y = 0, width = this.canvas.width, height = this.canvas.height) {
+    clear (fill = '#000', x = 0, y = 0, width = this.width, height = this.height) {
         this.ctx.fillStyle = fill
-        this.ctx.fillRect(0, 0, width, height)
+        this.ctx.fillRect(x, y, width, height)
     }
 
     render () {
