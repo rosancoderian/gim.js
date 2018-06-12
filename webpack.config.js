b9f410403e3420a214d9280fd6be9fd43ea693e9
rosancoderian <rosancoderian@gmail.com>
2018-06-12 14:44:43 +0700

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
-const path = require('path');
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
     entry: './src/gim.js',
     output: {
-        library: "gim",
-        libraryTarget: "umd",
-        libraryExport: "default",
+        library: 'gim',
+        libraryTarget: 'umd',
+        libraryExport: 'default',
         filename: 'gim.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    node: {
+        global: false
+    },
+    plugins: [new webpack.DefinePlugin({
+        global: 'window'
+    })]
 }
